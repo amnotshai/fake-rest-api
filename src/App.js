b36939207824971c2601efd122cd381c7851e366
amnotshai <cantilshai99@gmail.com>
2023-03-18 03:56:51 +0800

    made card and onclick function

--- src/App.js
+++ src/App.js
@@ -1,55 +1,93 @@
 import logo from './logo.svg';
 import './App.css';
 import { useEffect, useState } from 'react';
+import './components/Modal';
 
 function App() {
-const [post, setPosts]= useState('')
+const [posts, setPosts]= useState([])
+
+const [modal, setModal] = useState(false);
+  const [arrId, setCount] = useState(0);
+
+  const toggleModal = (event,key) => {
+    setModal(!modal);
+    
+    
+    
+   
+    
+  };
+  
+
+  if(modal) {
+    document.body.classList.add('active-modal')
+  } else {
+    document.body.classList.remove('active-modal')
+  }
+
 
 useEffect(() => {
   fetch('https://jsonplaceholder.typicode.com/posts')
     .then(response => {
       return response.json()
     })
-    .then(data =>{
-      console.log(data)
-    })
+    .then(value =>setPosts(value))
 }, [])
 
-const CardListItem = props => {
-  return (
-    <li>
-      <div
-        className="card-container"
-        style={{
-          width: "50%",
-          border: "solid 3px #d3d3d3",
-          margin: "10px auto"
-        }}
-      >
-        <h3>
-          <strong>{props.character.name}</strong>
-        </h3>
-        <p>{props.character.alterEgo}</p>
-        
-      
-      </div>
-    </li>
-  );
-};
-
-const CardList = () => {
-  return (
-    <ul style={{ listStyleType: "none" }}>
-      {posts.map(data => {
-        return <CardListItem data={data} key={data.id} />;
-      })}
-    </ul>
-  );
-};
+  
 
   return (
     <div className="App">
       <h1>Posts</h1>
+      <div style={{
+        display:'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        flexDirection:'row'
+        
+      }}>
+        <div>
+          {
+            posts.map((post)=>(
+              <div
+                key={post.id}
+                onClick={event=> {toggleModal(event,post.id);setCount(post.id-1)}}
+                className="card-container"
+                style={{
+                  width: "30%",
+                  border: "solid 3px #d3d3d3",
+                  margin: "10px auto"
+                }}
+              >
+                <h3>
+                  <strong>{post.title}</strong>
+                </h3>
+                <p>{post.body}</p>
+
+                <button onClick={event=> {toggleModal(event,post.id);setCount(post.id-1)}}>Click Me</button>
+              </div>
+            ))
+          }    
+        </div>
+
+      </div>
+
+      {modal && (
+        <div className="modal">
+          <div onClick={toggleModal} ></div>
+            <div className="modal-content">
+              <h4 align='left'>{window.location.host} says</h4>
+              <h5>You clicked {posts[arrId].title}</h5>
+              <button className="close-modal" onClick={toggleModal}>
+                OK
+              </button>
+            </div>
+        </div>
+      )}
+      
+
+      
+      
       
 
 
--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import "./Modal.css";
 
+
 export default function Modal() {
   const [modal, setModal] = useState(false);
 
