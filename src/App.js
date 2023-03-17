61b0f63df0e3c8e66328f460228119bf07bece70
amnotshai <cantilshai99@gmail.com>
2023-03-17 12:45:08 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+const [post, setPosts]= useState('')
+
+useEffect(() => {
+  fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(response => {
+      return response.json()
+    })
+    .then(data =>{
+      console.log(data)
+    })
+}, [])
+
+const CardListItem = props => {
+  return (
+    <li>
+      <div
+        className="card-container"
+        style={{
+          width: "50%",
+          border: "solid 3px #d3d3d3",
+          margin: "10px auto"
+        }}
+      >
+        <h3>
+          <strong>{props.character.name}</strong>
+        </h3>
+        <p>{props.character.alterEgo}</p>
+        
+      
+      </div>
+    </li>
+  );
+};
+
+const CardList = () => {
+  return (
+    <ul style={{ listStyleType: "none" }}>
+      {posts.map(data => {
+        return <CardListItem data={data} key={data.id} />;
+      })}
+    </ul>
+  );
+};
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Posts</h1>
+      
+
+
     </div>
   );
 }
